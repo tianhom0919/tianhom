33a83d637d553fe657310094874a37e166b609a1
tianhom0919 <ength091902@gmail.com>
2025-08-11 05:47:20 +0000

    Update config

--- th-ws/docusaurus.config.js
+++ th-ws/docusaurus.config.js
@@ -5,6 +5,7 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
+import { SlashIDProvider } from '@slashid/react';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
