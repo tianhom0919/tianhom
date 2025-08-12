43a0e8dde8d73f184d6d7dacea0e2f9db1fcac0b
tianhom0919 <ength091902@gmail.com>
2025-08-12 07:50:11 +0000

    docs: Change title

--- th-ws/docusaurus.config.js
+++ th-ws/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Welcome!",
+  title: "Welcome to my site!",
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
